0ec9187d82d2b7fc00f13ed987105f8931634382
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-13 12:37:49 +0000

    prints out area of rectangle

--- Main.java
+++ Main.java
@@ -1,15 +1,35 @@
+import java.util.Scanner;
 /**
  *
  * @author 
  */
 public class Main {
 
+  // a method to calculate the area of a rectangle given the width and the length
+  public static double areaOfRectangle( double width, double length){
+    double area = width * length;
+    return area;
+  }
+
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // using the method without a scanner
+    double answer = areaOfRectangle(2.0,5.0);
+    System.out.print(answer);
+
+    // using the method with a scanner
+    System.out.println("Please enter a width");
+    double width = input.nextDouble();
+    System.out.println("Please enter a length");
+    double length = input.nextDouble();
+    // calculate the area using the input
+    double area = areaOfRectangle(width, length);
     
   }
 }
